0c53351c2214bc9e820111d3159e9ca57bfd8335
Tony Blard <tonyblard55@gmail.com>
2024-07-11 15:37:14 +0200

    Feat : Create Button component Add ButtonProps type Export default Button

---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+type ButtonProps = {
+  onClick: () => void;
+  children: React.ReactNode;
+};
+const Button = ({ onClick, children }: ButtonProps) => (
+  <button onClick={onClick}>{children}</button>
+);
+
+export default Button;
