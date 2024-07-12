674d7dddb927e833727b6a508290769f16c84aa4
Tony Blard <tonyblard55@gmail.com>
2024-07-12 13:38:14 +0200

    feat : Add reset button in Counter

--- src/routes/Counter.tsx
+++ src/routes/Counter.tsx
@@ -3,8 +3,9 @@ import Button from "../components/Button";
 
 const Counter = () => {
   const [count, setCount] = useState<number>(0);
-  const increment = (incrementNumber: number) =>
+  const increment: (incrementNumber: number) => void = (incrementNumber) =>
     setCount((count) => count + incrementNumber);
+  const reset: () => void = () => setCount(0);
   return (
     <>
       <h1 className="text-3xl font-bold text-center mb-3">Counter</h1>
@@ -16,6 +17,7 @@ const Counter = () => {
       <div className="flex gap-4 justify-center">
         <Button onClick={() => increment(1)}>Add 1 </Button>
         <Button onClick={() => increment(3)}>Add 3</Button>
+        <Button onClick={() => reset()}>Reset</Button>
       </div>
     </>
   );
