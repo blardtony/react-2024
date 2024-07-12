a12cf5610ed7f97328c8860b20b4e6056d9141b4
Tony Blard <tonyblard55@gmail.com>
2024-07-12 09:27:58 +0200

    feat : Styling button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,12 @@ type ButtonProps = {
   children: React.ReactNode;
 };
 const Button = ({ onClick, children }: ButtonProps) => (
-  <button onClick={onClick}>{children}</button>
+  <button
+    className="text-white bg-violet-700 hover:bg-violet-800 focus:ring-2 focus:ring-violet-300 focus:outline-none font-medium rounded px-4 py-2"
+    onClick={onClick}
+  >
+    {children}
+  </button>
 );
 
 export default Button;
