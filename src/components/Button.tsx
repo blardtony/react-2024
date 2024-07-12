aaf165c70a9f59d3429dcc10aec1f09e18d2520c
Tony Blard <tonyblard55@gmail.com>
2024-07-12 14:26:50 +0200

    feat : Add type props onClick can be undefined

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,13 @@
 type ButtonProps = {
-  onClick: () => void;
+  type?: "button" | "submit";
+  onClick?: () => void;
   children: React.ReactNode;
 };
-const Button = ({ onClick, children }: ButtonProps) => (
+const Button = ({ type, onClick, children }: ButtonProps) => (
   <button
     className="text-white bg-violet-700 hover:bg-violet-800 focus:ring-2 focus:ring-violet-300 focus:outline-none font-medium rounded px-4 py-2"
     onClick={onClick}
+    type={type || "button"}
   >
     {children}
   </button>
