9d1e59e38a9e08e22b75cccc5c58c519343dd2d0
Tony Blard <tonyblard55@gmail.com>
2024-07-12 14:27:20 +0200

    feat : Add Todo in router

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Index from "./routes";
 import Counter from "./routes/Counter";
+import Todo from "./routes/Todo";
 
 export const router = createBrowserRouter([
   {
@@ -10,5 +11,9 @@ export const router = createBrowserRouter([
   {
     path: "/counter",
     element: <Counter />,
+  },
+  {
+    path: "/todo",
+    element: <Todo />,
   },
 ]);
